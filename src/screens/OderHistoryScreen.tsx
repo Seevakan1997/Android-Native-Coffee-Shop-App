0a149517b8dbd7cd777323d48f664ed2a7a13aa4
Seevakan1997 <seevakan1@gmail.com>
2024-03-28 21:40:22 +0530

    add navigators

---
+++ src/screens/OderHistoryScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OderHistoryScreen = () => {
+  return (
+    <View>
+      <Text>OderHistoryScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
+
+export default OderHistoryScreen
